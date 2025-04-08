df6e3e63e473b97b711c0c2377468e16d3be4673
Victor Junqueira <victorjunqueira.prog@gmail.com>
2025-04-08 15:02:12 -0300

    Add files via upload

---
+++ csharp/Car.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1;
+
+public class Car
+{
+    public static List<Car> AllCars { get; } = new();
+
+    public string Brand { get; set; }
+    public string Color { get; set; }
+    public string Model { get; set; }
+    public int MaxSpeed { get; set; }
+
+    public Car(string brand, string color, string model, int maxSpeed)
+    {
+        Brand = brand;
+        Color = color;
+        Model = model;
+        MaxSpeed = maxSpeed;
+
+        AllCars.Add(this);
+    }
+}
